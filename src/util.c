6b74073769176cd2df646df2b2bfa21811eca1dc
dedmen <dedmenmiller@googlemail.com>
2015-10-15 21:41:13 +0200

    Update util.c
    Fixed Indentation

--- src/util.c
+++ src/util.c
@@ -52,9 +52,11 @@ void asmlog(int level, const char* format, ...)
 	}
 
 	va_start(ap, format);
+	
 	if (level >= LOG_ERR) {
-	vsyslog(level, format, ap);
+		vsyslog(level, format, ap);
 	}
+	
 	if (level <= loglevel) {
 		fprintf(os,"asmdll: ");
 		vfprintf(os, format, ap);
